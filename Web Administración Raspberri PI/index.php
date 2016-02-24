c05f6bff0ee5770df211f575a7bcfdf3327e0a68
nerviozzo <raupulus@gmail.com>
2016-02-24 01:10:11 +0100

    pruebas

--- Web Administración Raspberri PI/index.php
+++ Web Administración Raspberri PI/index.php
@@ -6,56 +6,12 @@
 		<LINK rel="shortcut icon" href="./images/favicon.png"/>
 		<LINK rel="stylesheet" href="./style.css"/>
 		<SCRIPT src="./scripts.js"></SCRIPT>
-<?php
-// Funciones PHP del pin GPIO 2
-	if ($_POST[Activar2]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/2/activar.py");
-		echo $a;
-	}
 
-	if ($_POST[Desactivar2]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/2/apagar.py");
-		echo $a;
-	}
-
-	if ($_POST[UNparpadeo2]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/2/1parpadeo.py");
-		echo $a;
-	}
-
-	if ($_POST[Parpadear2]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/2/parpadear.py");
-		echo $a;
-	}
-// Fin de las funciónes del pin GPIO 2
-		
-// Funciones PHP del pin GPIO 3
-	if ($_POST[Activar3]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/3/activar.py");
-		echo $a;
-	}
-
-	if ($_POST[Desactivar3]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/3/apagar.py");
-		echo $a;
-	}
-
-	if ($_POST[UNparpadeo3]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/3/1parpadeo.py");
-		echo $a;
-	}
-
-	if ($_POST[Parpadear3]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/3/parpadear.py");
-		echo $a;
-	}
-// Fin de las funciónes del pin GPIO 3
-?>
 	</HEAD>
 	
 	<BODY id="pines">
 		<H1>WEB DE CONTROL RaspberryPI</H1>
-<FORM action="" method="post">		
+<FORM action="funciones.php" method="post">		
 		<DIV id="cajamenuprincipal">
 			<NAV id="menuprincipal">
 				<UL>
