f2e541bf964a8f14a856087ae02a8ea62cacaa9c
nerviozzo <raupulus@gmail.com>
2016-02-24 15:56:28 +0100

    Terminado y funcionando correctamente. Ahora apaga todos los led encendidos antes de empezar, luego parpadea 3 cada un segundo durante 10 repeticiones

--- Web Administración Raspberri PI/GPIO/TODOS/parpadeoAleatorio.py
+++ Web Administración Raspberri PI/GPIO/TODOS/parpadeoAleatorio.py
@@ -47,18 +47,48 @@ GPIO.setup(20, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
-#Bucle que activa de forma desordenada las salidas, máximo 3 a la vez y 5 veces
+#Asigno valor bajo para apagar todos
+GPIO.output(2, GPIO.LOW)
+GPIO.output(3, GPIO.LOW)
+GPIO.output(4, GPIO.LOW)
+GPIO.output(5, GPIO.LOW)
+GPIO.output(6, GPIO.LOW)
+GPIO.output(7, GPIO.LOW)
+GPIO.output(8, GPIO.LOW)
+GPIO.output(9, GPIO.LOW)
+GPIO.output(10, GPIO.LOW)
+GPIO.output(11, GPIO.LOW)
+GPIO.output(12, GPIO.LOW)
+GPIO.output(13, GPIO.LOW)
+GPIO.output(14, GPIO.LOW)
+GPIO.output(15, GPIO.LOW)
+GPIO.output(16, GPIO.LOW)
+GPIO.output(17, GPIO.LOW)
+GPIO.output(18, GPIO.LOW)
+GPIO.output(19, GPIO.LOW)
+GPIO.output(20, GPIO.LOW)
+GPIO.output(21, GPIO.LOW)
+GPIO.output(22, GPIO.LOW)
+
+#Bucle que activa de forma desordenada las salidas, máximo 3 a la vez y 10 veces
 # GPIO.setup($variabledelarray, GPIO.OUT)
 i = 1
-while i <= 5:
-	print (GPIO.setup(aleatorio1, GPIO.OUT))
-	print aleatorio1
-	print aleatorio2
-	print aleatorio3
-	aleatorio1 = random.randint(2, 11)
-	aleatorio2 = random.randint(2, 11)
-	aleatorio3 = random.randint(2, 11)
-	i += 1
+while i <= 10:
+        GPIO.output( aleatorio1, GPIO.HIGH)
+        GPIO.output( aleatorio2, GPIO.HIGH)
+        GPIO.output( aleatorio3, GPIO.HIGH)
+        sleep(1)
+        GPIO.output( aleatorio1, GPIO.LOW)
+        GPIO.output( aleatorio2, GPIO.LOW)
+        GPIO.output( aleatorio3, GPIO.LOW)
+        print aleatorio1
+        print aleatorio2
+        print aleatorio3
+        aleatorio1 = random.randint(2, 11)
+        aleatorio2 = random.randint(2, 11)
+        aleatorio3 = random.randint(2, 11)
+        sleep(1)
+        i += 1
 print("Terminado")
 
 #Limpiar GPIO
