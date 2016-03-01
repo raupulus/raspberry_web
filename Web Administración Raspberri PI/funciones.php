cb35974efac07ed29ab5db2a93e2bfc27ac3d9a6
fryntiz <fryntiz@gmail.com>
2016-03-01 17:34:29 +0100

    Ruta relativa hacia los scripts desde la raíz de la web (index.html)

--- Web Administración Raspberri PI/funciones.php
+++ Web Administración Raspberri PI/funciones.php
@@ -47,405 +47,405 @@
 
 // Funciones PHP del pin GPIO 4
 	if ($_POST[Activar4]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/4/activar.py");
+		$a=exec("sudo python ./GPIO/4/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar4]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/4/apagar.py");
+		$a=exec("sudo python ./GPIO/4/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo4]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/4/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/4/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear4]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/4/parpadear.py");
+		$a=exec("sudo python ./GPIO/4/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 4
 
 // Funciones PHP del pin GPIO 5
 	if ($_POST[Activar5]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/5/activar.py");
+		$a=exec("sudo python ./GPIO/5/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar5]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/5/apagar.py");
+		$a=exec("sudo python ./GPIO/5/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo5]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/5/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/5/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear5]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/5/parpadear.py");
+		$a=exec("sudo python ./GPIO/5/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 5
 
 // Funciones PHP del pin GPIO 6
 	if ($_POST[Activar6]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/6/activar.py");
+		$a=exec("sudo python ./GPIO/6/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar6]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/6/apagar.py");
+		$a=exec("sudo python ./GPIO/6/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo6]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/6/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/6/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear6]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/6/parpadear.py");
+		$a=exec("sudo python ./GPIO/6/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 6
 
 // Funciones PHP del pin GPIO 7
 	if ($_POST[Activar7]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/7/activar.py");
+		$a=exec("sudo python ./GPIO/7/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar7]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/7/apagar.py");
+		$a=exec("sudo python ./GPIO/7/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo7]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/7/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/7/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear7]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/7/parpadear.py");
+		$a=exec("sudo python ./GPIO/7/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 7
 
 // Funciones PHP del pin GPIO 8
 	if ($_POST[Activar8]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/8/activar.py");
+		$a=exec("sudo python ./GPIO/8/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar8]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/8/apagar.py");
+		$a=exec("sudo python ./GPIO/8/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo8]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/8/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/8/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear8]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/8/parpadear.py");
+		$a=exec("sudo python ./GPIO/8/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 8
 
 // Funciones PHP del pin GPIO 9
 	if ($_POST[Activar9]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/9/activar.py");
+		$a=exec("sudo python ./GPIO/9/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar9]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/9/apagar.py");
+		$a=exec("sudo python ./GPIO/9/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo9]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/9/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/9/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear9]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/9/parpadear.py");
+		$a=exec("sudo python ./GPIO/9/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 9
 
 // Funciones PHP del pin GPIO 10
 	if ($_POST[Activar10]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/10/activar.py");
+		$a=exec("sudo python ./GPIO/10/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar10]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/10/apagar.py");
+		$a=exec("sudo python ./GPIO/10/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo10]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/10/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/10/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear10]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/10/parpadear.py");
+		$a=exec("sudo python ./GPIO/10/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 10
 
 // Funciones PHP del pin GPIO 11
 	if ($_POST[Activar11]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/11/activar.py");
+		$a=exec("sudo python ./GPIO/11/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar11]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/11/apagar.py");
+		$a=exec("sudo python ./GPIO/11/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo11]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/11/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/11/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear11]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/11/parpadear.py");
+		$a=exec("sudo python ./GPIO/11/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 11
 
 // Funciones PHP del pin GPIO 12
 	if ($_POST[Activar12]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/12/activar.py");
+		$a=exec("sudo python ./GPIO/12/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar12]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/12/apagar.py");
+		$a=exec("sudo python ./GPIO/12/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo12]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/12/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/12/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear12]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/12/parpadear.py");
+		$a=exec("sudo python ./GPIO/12/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 12
 
 // Funciones PHP del pin GPIO 13
 	if ($_POST[Activar13]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/13/activar.py");
+		$a=exec("sudo python ./GPIO/13/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar13]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/13/apagar.py");
+		$a=exec("sudo python ./GPIO/13/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo13]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/13/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/13/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear13]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/13/parpadear.py");
+		$a=exec("sudo python ./GPIO/13/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 13
 
 // Funciones PHP del pin GPIO 14
 	if ($_POST[Activar14]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/14/activar.py");
+		$a=exec("sudo python ./GPIO/14/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar14]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/14/apagar.py");
+		$a=exec("sudo python ./GPIO/14/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo14]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/14/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/14/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear14]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/14/parpadear.py");
+		$a=exec("sudo python ./GPIO/14/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 14
 
 // Funciones PHP del pin GPIO 15
 	if ($_POST[Activar15]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/15/activar.py");
+		$a=exec("sudo python ./GPIO/15/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar15]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/15/apagar.py");
+		$a=exec("sudo python ./GPIO/15/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo15]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/15/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/15/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear15]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/15/parpadear.py");
+		$a=exec("sudo python ./GPIO/15/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 15
 
 // Funciones PHP del pin GPIO 16
 	if ($_POST[Activar16]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/16/activar.py");
+		$a=exec("sudo python ./GPIO/16/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar16]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/16/apagar.py");
+		$a=exec("sudo python ./GPIO/16/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo16]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/16/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/16/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear16]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/16/parpadear.py");
+		$a=exec("sudo python ./GPIO/16/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 16
 
 // Funciones PHP del pin GPIO 17
 	if ($_POST[Activar17]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/17/activar.py");
+		$a=exec("sudo python ./GPIO/17/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar17]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/17/apagar.py");
+		$a=exec("sudo python ./GPIO/17/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo17]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/17/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/17/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear17]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/17/parpadear.py");
+		$a=exec("sudo python ./GPIO/17/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 17
 
 // Funciones PHP del pin GPIO 18
 	if ($_POST[Activar18]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/18/activar.py");
+		$a=exec("sudo python ./GPIO/18/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar18]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/18/apagar.py");
+		$a=exec("sudo python ./GPIO/18/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo18]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/18/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/18/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear18]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/18/parpadear.py");
+		$a=exec("sudo python ./GPIO/18/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 18
 
 // Funciones PHP del pin GPIO 19
 	if ($_POST[Activar19]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/19/activar.py");
+		$a=exec("sudo python ./GPIO/19/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar19]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/19/apagar.py");
+		$a=exec("sudo python ./GPIO/19/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo19]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/19/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/19/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear19]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/19/parpadear.py");
+		$a=exec("sudo python ./GPIO/19/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 19
 
 // Funciones PHP del pin GPIO 20
 	if ($_POST[Activar20]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/20/activar.py");
+		$a=exec("sudo python ./GPIO/20/activar.py");
 		echo $a;
 	}
 
 	if ($_POST[Desactivar20]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/20/apagar.py");
+		$a=exec("sudo python ./GPIO/20/apagar.py");
 		echo $a;
 	}
 
 	if ($_POST[UNparpadeo20]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/20/1parpadeo.py");
+		$a=exec("sudo python ./GPIO/20/1parpadeo.py");
 		echo $a;
 	}
 
 	if ($_POST[Parpadear20]) { 
-		$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/20/parpadear.py");
+		$a=exec("sudo python ./GPIO/20/parpadear.py");
 		echo $a;
 	}
 // Fin de las funciónes del pin GPIO 20
 
 //Encender todos los pines
 	if ($_POST[todoON]) { 
-			$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/TODOS/activar.py");
+			$a=exec("sudo python ./GPIO/TODOS/activar.py");
 	echo $a;
 	}
 
 //Apagar todos los pines
 	if ($_POST[todoOFF]) { 
-			$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/TODOS/apagar.py");
+			$a=exec("sudo python ./GPIO/TODOS/apagar.py");
 	echo $a;
 	}
 	
 //Parpadear en orden todos los pines
 	if ($_POST[parpadeoORDEN]) { 
-			$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/TODOS/parpadeoOrdenado.py");
+			$a=exec("sudo python ./GPIO/TODOS/parpadeoOrdenado.py");
 	echo $a;
 	}
 
 //Parpadear aleatorio todos los pines
 	if ($_POST[parpadeoALEATORIO]) { 
-			$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/TODOS/parpadeoAleatorio.py");
+			$a=exec("sudo python ./GPIO/TODOS/parpadeoAleatorio.py");
 	echo $a;
 	}
 
 //Parpadear todos los pines a la vez
 	if ($_POST[parpadeoTODO]) { 
-			$a=exec("sudo python /var/www/webRaspberriPI/Web\ Administración\ Raspberri\ PI/GPIO/TODOS/parpadeoTodo.py");
+			$a=exec("sudo python ./GPIO/TODOS/parpadeoTodo.py");
 	echo $a;
 		}
 
