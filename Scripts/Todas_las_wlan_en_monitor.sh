0e0a76c5397ea5f43ccc7bdcbc7ce9101abf9f1b
fryntiz <fryntiz@gmail.com>
2017-05-28 03:57:16 +0200

    Estructura basica

--- Scripts/Todas_las_wlan_en_monitor.sh
+++ Scripts/Todas_las_wlan_en_monitor.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#Ver dispositivos de red en el equipo
+REDES=`ls  /sys/class/net/ | tr -s " " | grep wlan`
+MACS=""
+
+#Mostrar Redes
+echo "Redes detectadas:"
+
+#Bucle para configurar cada red en modo monitor
+for i in $REDES; do
+	#ifconfig $i
+	echo "$i"
+done
+
