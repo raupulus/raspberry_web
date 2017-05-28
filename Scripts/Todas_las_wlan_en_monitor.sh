ea95890a611ece1c0e6b1b469379d9c886884e01
fryntiz <fryntiz@gmail.com>
2017-05-28 21:59:10 +0200

    Agregadas dependencias

--- Scripts/Todas_las_wlan_en_monitor.sh
+++ Scripts/Todas_las_wlan_en_monitor.sh
@@ -22,9 +22,13 @@ function actualizarRedes() {
 #Bucle para configurar cada red en modo monitor
 for i in $REDES; do
 	#ifconfig $i
+
 	echo "$i"
 done
 
 #Guardar en array las direcciones MAC de cada red y en otro el nombre de esta sin monitor y con monitor
 #Las tarjetas wifis con menos alcance escanean con más frecuencia
 #Registrar todo en log
+
+#DEPENDENCIAS:
+# aircrack-ng macchanger
