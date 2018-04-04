402bc2c97846a91502a4e548524e738b0c0d56dd
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-04 13:07:50 +0200

    Personalizo mensaje de error

--- views/site/error.php
+++ views/site/error.php
@@ -17,11 +17,35 @@ $this->title = $name;
         <?= nl2br(Html::encode($message)) ?>
     </div>
 
+    <h2>Ha ocurrido un error</h2>
+
+    <h3>
+        <span class="mensaje-error">atención>Umhh ehh... !Quieto ahí!</span>
+    </h3>
+
     <p>
-        The above error occurred while the Web server was processing your request.
+        Si estás viendo esta ventana pueden haber ocurrido varias cosas:
+        <ul>
+            <li>
+                Tenemos un error y en ese caso agradeceriamos que lo reportes
+                desde la <a href="site/contact" title="contacto">la página
+                de contacto</a> de esta web.
+            </li>
+
+            <li>
+                Has introducido mal algún parámetro o ruta, revísalo.
+            </li>
+
+            <li>
+                Estás intentando hacer cosas malas. Ten en cuenta que será
+                registrado el error y en caso de repetirse lo investigaremos.
+            </li>
+        </ul>
     </p>
+
     <p>
-        Please contact us if you think this is a server error. Thank you.
+        Por favor, contáctanos si ves cualquier error o comportamiento extraño
+        para que podamos mejorar la calidad del servicio.
     </p>
 
 </div>
