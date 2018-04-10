a034f42acbad0bbc4fe7341a582e72ea76a4db36
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-10 22:04:40 +0200

    Planteando función para enviar

--- views/site/gpio.php
+++ views/site/gpio.php
@@ -3,6 +3,7 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = 'GPIO';
 $this->params['breadcrumbs'][] = $this->title;
@@ -16,55 +17,114 @@ $this->params['breadcrumbs'][] = $this->title;
         Este es el menú dónde puedes depurar los pines.
     </p>
 
-    <form action="#" method="post">
-        <table class="tablatodos">
-            <tr>
-                <th>TODO<br />ON</th>
-                <th>TODO<br />OFF</th>
-                <th>Parpadear<br />en orden</th>
-                <th>Parpadear<br />aleatorio</th>
-                <th>Parpadear<br />TODO</th>
-            </tr>
-            <tr>
-                <td class="pulsador">
-                    <input class="activar" type="submit" name="todoON" value="todoON"/>
-                </td>
-                <td class="pulsador">
-                    <input class="desactivar" type="submit" name="todoOFF" value="todoOFF"/>
-                </td>
-                <td class="pulsador">
-                    <input class="activar" type="submit" name="parpadeoORDEN" value="parpadeoORDEN"/>
-                </td>
-                <td class="pulsador">
-                    <input class="activar" type="submit" name="parpadeoALEATORIO" value="parpadeoALEATORIO"/>
-                </td>
-                <td class="pulsador">
-                    <input class="activar" type="submit" name="parpadeoTODO" value="parpadeoTODO"/>
-                </td>
-            </tr>
-        </table>
+    <table class="tablatodos">
+        <tr>
+            <th>TODO<br />ON</th>
+            <th>TODO<br />OFF</th>
+            <th>Parpadear<br />en orden</th>
+            <th>Parpadear<br />aleatorio</th>
+            <th>Parpadear<br />TODO</th>
+        </tr>
+        <tr>
+            <td class="pulsador">
+                <input class="activar" type="submit" name="todoON" value="todoON"/>
+            </td>
+            <td class="pulsador">
+                <input class="desactivar" type="submit" name="todoOFF" value="todoOFF"/>
+            </td>
+            <td class="pulsador">
+                <input class="activar" type="submit" name="parpadeoORDEN" value="parpadeoORDEN"/>
+            </td>
+            <td class="pulsador">
+                <input class="activar" type="submit" name="parpadeoALEATORIO" value="parpadeoALEATORIO"/>
+            </td>
+            <td class="pulsador">
+                <input class="activar" type="submit" name="parpadeoTODO" value="parpadeoTODO"/>
+            </td>
+        </tr>
+    </table>
 
-        <table class="tablaprincipal">
-            <tr>
-                <th>PIN</th>
-                <th>ACTIVAR</th>
-                <th>DESACTIVAR</th>
-                <th>1PARPADEO</th>
-                <th>PARPADEAR</th>
-                <th>ESTADO</th>
-            </tr>
+    <table class="tablaprincipal">
+        <tr>
+            <th>PIN</th>
+            <th>ACTIVAR</th>
+            <th>DESACTIVAR</th>
+            <th>1PARPADEO</th>
+            <th>PARPADEAR</th>
+            <th>ESTADO</th>
+        </tr>
 
-            <?php foreach (range(1,40) as $n): ?>
-                <tr>
+        <?php foreach (range(1,40) as $n): ?>
+            <tr>
+                <?= Html::beginForm([
+                    'site/gpio',
+                ],
+                    'post',
+                    [
+                        'enableAjaxValidation'=>false,
+                        'enctype' => 'multipart/form-data',
+                        'class' => 'form-horizontal col-lg-6 col-lg-offset-3 ',
+                        'id' => 'gpio'.$n.'-form',
+                        //'afterValidate'=>'js:enviar',
+                        //'onsubmit'=>"",
+                    ]
+                    ); ?>
                     <td><?= $n; ?></td>
-                    <td class="pulsador"><input class="activar" type="submit" name="<?= $n; ?>" value="<?= $n; ?>"/></td>
-                    <td class="pulsador"><input class="desactivar" type="submit" name="<?= $n; ?>" value="<?= $n; ?>"/></td>
-                    <td class="pulsador"><input class="activar" type="submit" name="<?= $n; ?>" value="<?= $n; ?>"/></td>
-                    <td class="pulsador"><input class="activar" type="submit" name="<?= $n; ?>" value="<?= $n; ?>"/></td>
-                    <td>¿?</td>
-                </tr>
 
-            <?php endforeach; ?>
-        </table>
-    </form>
+                    <input type="text" name="pin" value="<?= $n ?>" hidden />
+
+                    <td class="pulsador">
+                        <input class="activar" type="button"
+                               name="<?= $n; ?>" value="<?= $n; ?>"/>
+                    </td>
+                    <td class="pulsador">
+                        <input class="desactivar" type="submit"
+                               name="<?= $n; ?>" value="<?= $n; ?>"/>
+                    </td>
+                    <td class="pulsador">
+                        <input class="activar" type="submit"
+                               name="<?= $n; ?>" value="<?= $n; ?>"/>
+                    </td>
+                    <td class="pulsador">
+                        <input class="activar" type="submit"
+                               name="<?= $n; ?>" value="<?= $n; ?>"/>
+                    </td>
+                    <td>¿?</td>
+                <?= Html::endForm() ?>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </div>
+
+
+<script type="text/javascript">
+    window.onload = {
+        $.('input[type="button"]').click(enviar);
+    }
+
+    function enviar(){
+        console.log('Entra en función enviar()');
+        /*
+        if(!hasError){
+            str = $('#'+formulario).serialize();
+            $.ajax({
+                type: "POST",
+                url: "<?= Url::to('site/gpio'); ?>",
+                data: str,
+                beforeSend : function() {
+                    //$("#login").attr("disabled",true);
+                },
+                success:function(data){
+                    //document.getElementById('respuesta').innerHTML = data;
+                    //$("#login").attr("disabled",false);
+                    alert('encendido');
+                },
+                error: function(data) { // if error occured
+                    alert('error');
+                }
+            });
+        }
+        */
+        return false;
+    }
+</script>
