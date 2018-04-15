a888ecbb8b5fdb9a90d63b0994e3461a12b8b362
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-15 23:18:11 +0200

    Elimino campos innecesarios del formulario

--- views/usuarios/_form.php
+++ views/usuarios/_form.php
@@ -20,16 +20,10 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'auth_key')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'rol')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
